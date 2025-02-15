f22f467a46c839197784b9dd9f952ba45bc4d252
issam Benmaina <issambenmaina92@gmail.com>
2025-02-15 16:04:53 +0100

    Update SignIn styles and adjust Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,9 @@
 import type { Config } from "tailwindcss";
 import fluid, { extract } from 'fluid-tailwind'
 
-
 export default {
   content: {
     files: [
-
       "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
       "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
       "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
